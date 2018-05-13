9b8d4f1588618f4c7bb545a2349f339aaf5cfe1c
Katy Muhlrad <kmuhlrad@gmail.com>
2018-05-13 19:29:38 -0400

    cleaned up explore drawer code

--- modules/spartan/manipulation/explore_drawer.py
+++ modules/spartan/manipulation/explore_drawer.py
@@ -21,11 +21,10 @@ import pdb
 class ExploreDrawer(object):
 
     def __init__(self, start_x, start_y, desired_z, drawer_width, drawer_height, end_effector_width, end_effector_height, num_waypoints = 0):
-        self.jointNames = (u'iiwa_joint_1', u'iiwa_joint_2', u'iiwa_joint_3', u'iiwa_joint_4', u'iiwa_joint_5', u'iiwa_joint_6', u'iiwa_joint_7')
-        self.robotService = spartanROSUtils.RobotService(self.jointNames)
-        self.currentJointPosition = [0]*len(self.jointNames)
+        self.robot_service = spartanROSUtils.RobotService.makeKukaRobotService()
+        self.cur_joint_position = [0]*self.robot_service.numJoints
 
-        self.maxJointDegreesPerSecond = 15
+        self.max_joint_deg_per_sec = 15
 
         self.start_x = start_x
         self.start_y = start_y
@@ -33,80 +32,55 @@ class ExploreDrawer(object):
 
         self.should_stop = False
         self.should_switch_modes = False
+        self.in_contact = False
 
         self.num_rows, self.num_cols, self.cell_width, self.cell_height = self.calcGridInfo(drawer_width, drawer_height, end_effector_width, end_effector_height)
 
         grid_goal_points, directions = self.calcGridGoalPoints(num_waypoints)
+        all_cartesian_knot_points = self.calcCartesianKnotPoints(grid_goal_points)
+        all_joint_positions = [self.getJointPositions(point, directions[i]) for (i, point) in enumerate(all_cartesian_knot_points)]
 
-        #print grid_goal_points
+        self.good_joint_positions = [point for point in all_joint_positions if point is not None]
+        self.good_directions = [directions[i] for i in range(len(all_joint_positions)) if all_joint_positions[i] is not None]
+        self.good_cartesian_points = [all_cartesian_knot_points[i] for i in range(len(all_joint_positions)) if all_joint_positions[i] is not None]
+        
+        self.cur_cartesian_point = self.good_cartesian_points[0]
+        self.cur_direction = self.good_directions[0]
 
-        knot_points = self.calcCartesianKnotPoints(grid_goal_points)
-
-        self.all_knot_points = [self.getJointPositions(point, directions[i]) for (i, point) in enumerate(knot_points)]
-        self.joint_knot_points = [point for point in self.all_knot_points if point is not None]
-        self.directions = [directions[i] for i in range(len(self.all_knot_points)) if self.all_knot_points[i] is not None]
-        self.all_cartesian_points = [knot_points[i] for i in range(len(self.all_knot_points)) if self.all_knot_points[i] is not None]
-        self.cur_cart_point = self.all_cartesian_points[0]
-        self.cur_direction = self.directions[0]
-
-        self.not_hit = True
-
-        #pdb.set_trace()
-
-        print "total knot points:", len(self.all_knot_points), "feasible knot points:", len(self.joint_knot_points)
+        print "total knot points:", len(all_joint_positions), "feasible knot points:", len(self.good_joint_positions)
 
         rospy.Subscriber("/joint_states", sensor_msgs.msg.JointState, self.storeCurrentJointPosition)
         rospy.Subscriber("/stop", std_msgs.msg.Bool, self.stopCurrentPoint)
-        rospy.Subscriber("/hit_object", std_msgs.msg.Bool, self.switchToFindObject)
+        rospy.Subscriber("/hit_object", std_msgs.msg.Bool, self.switchToFindObjectMode)
+
+    def sendStopRobotCommand(self):
+        rospy.loginfo("stopping robot")
+        lcm_msg = robot_plan_t()
+        lcm_msg.utime = 0
+        lc = lcm.LCM()
+        lc.publish("STOP", lcm_msg.encode())
 
     def storeCurrentJointPosition(self, msg):
-        self.currentJointPosition = msg.position
+        self.cur_joint_position = msg.position
 
-        if abs(msg.effort[0]) > 7000 and abs(msg.effort[2]) > 7000 and self.not_hit:
+        contact_force_threshold = 7000
+
+        if abs(msg.effort[0]) > contact_force_threshold and abs(msg.effort[2]) > contact_force_threshold and not self.in_contact:
+            rospy.loginfo("hit something")
+            self.sendStopRobotCommand()
+
             self.should_switch_modes = True
-            # self.should_stop = True
-            # self.not_hit = False
-
-            rospy.loginfo("hit something, stopping robot")
-            lcm_msg = robot_plan_t()
-            lcm_msg.utime = 0
-            lc = lcm.LCM()
-            lc.publish("STOP", lcm_msg.encode())
-
-            # new_points = self.calcNextFindObjectPoints(self.cur_cart_point, self.cur_direction)
-            # for point in new_points:
-            #     if self.should_stop:
-            #         break
-            #     self.robotService.moveToJointPosition(point, self.maxJointDegreesPerSecond)
-            #     rospy.sleep(0.2)
-            # self.not_hit = True
+            self.in_contact = True
 
     def stopCurrentPoint(self, msg):
         if msg.data:
-            rospy.loginfo("stopping robot")
-            lcm_msg = robot_plan_t()
-            lcm_msg.utime = 0
-            lc = lcm.LCM()
-            lc.publish("STOP", lcm_msg.encode())
-
+            self.sendStopRobotCommand()
             self.should_stop = True
 
-    def switchToFindObject(self, msg):
-        # gripper contact: effort[2] becomes 10000, 0,1 also become large (order 9000 instead of 2/90)
+    def switchToFindObjectMode(self, msg):
         if msg.data:
-            rospy.loginfo("stopping robot")
-            lcm_msg = robot_plan_t()
-            lcm_msg.utime = 0
-            lc = lcm.LCM()
-            lc.publish("STOP", lcm_msg.encode())
-
+            self.sendStopRobotCommand()
             self.should_switch_modes = True
-
-            # new_points = self.calcNextFindObjectPoints(self.cur_cart_point, self.cur_direction)
-            # for point in new_points:
-            #     if self.should_stop:
-            #         break
-            #     self.robotService.moveToJointPosition(point, self.maxJointDegreesPerSecond)
 
     def calcGridInfo(self, drawer_width, drawer_height, end_effector_width, end_effector_height):
         num_cols = int(drawer_width / end_effector_width)
@@ -194,47 +168,34 @@ class ExploreDrawer(object):
     def calcNextFindObjectPoints(self, current_cartesian_point, direction):
         next_cartesian_points = []
 
-        i = 1
-        ik_failed = True
-        while ik_failed:
-            # 2 cells away from the contact point
-            next_cartesian_points = []
+        num_grid_cells = 1
+        cells_away = 2
+        cells_towards = 3
 
-            next_point = list(current_cartesian_point)
-            for j in range(2):
-                next_point[1] = next_point[1] - direction*i*self.cell_height
-                next_cartesian_points.append(tuple(next_point))
-                self.cur_cart_point = tuple(next_point)
+        next_point = list(current_cartesian_point)
 
-            # 2 cells down from the contact points
-            next_point[0] = next_point[0] + i*self.cell_width
+        # First go away from the object
+        for j in range(cells_away):
+            next_point[1] = next_point[1] - direction*num_grid_cells*self.cell_height
             next_cartesian_points.append(tuple(next_point))
-            self.cur_cart_point = tuple(next_point)
+            self.cur_cartesian_point = tuple(next_point)
 
-            # 4 cells towards the object at the new x-value
-            for j in range(3):
-                next_point[1] = next_point[1] + direction*i*self.cell_height
-                next_cartesian_points.append(tuple(next_point))
-                self.cur_cart_point = tuple(next_point)
+        # Then go down
+        next_point[0] = next_point[0] + num_grid_cells*self.cell_width
+        next_cartesian_points.append(tuple(next_point))
+        self.cur_cartesian_point = tuple(next_point)
 
-            # self.cur_cart_point = tuple(next_point)
-            print next_cartesian_points
+        # Finally, go back towards the object
+        for j in range(cells_towards):
+            next_point[1] = next_point[1] + direction*num_grid_cells*self.cell_height
+            next_cartesian_points.append(tuple(next_point))
+            self.cur_cartesian_point = tuple(next_point)
 
-            joint_positions = [self.getJointPositions(point, direction) for point in next_cartesian_points]
-
-            for point in joint_positions:
-                if point is not None:
-                    ik_failed = False
-                else:
-                    ik_failed = True
-                    i += 1
-                    break
-
-        return joint_positions
+        return next_cartesian_points
 
     '''
-    cartesian_point is a list of 3 floats for x, y, and z (ex: [0.1, 0.2, 0.3])
-    This format can be changed to reflect how the points are estimated from the point cloud.
+    cartesian_point is a list/tuple of 3 floats for x, y, and z (ex: [0.1, 0.2, 0.3])
+    direction is an integer, +1 or -1
     '''
     def getJointPositions(self, cartesian_point, direction):
         pose = geometry_msgs.msg.Pose()
@@ -242,7 +203,8 @@ class ExploreDrawer(object):
         pose.position.y = cartesian_point[1]
         pose.position.z = cartesian_point[2]
 
-        # The gripper should be pointing straight down.
+        # The gripper should be pointing straight down with the GelSight
+        # towards the center of the table.
         if direction >= 0:
             quat = quaternion_from_euler(pi/7, pi/2, 0)
         else:
@@ -256,54 +218,50 @@ class ExploreDrawer(object):
         poseStamped.pose = pose
         poseStamped.header.frame_id = "base"
 
-        response = self.robotService.runIK(poseStamped)
+        response = self.robot_service.runIK(poseStamped)
         if not response.success:
-            #rospy.loginfo("ik was not successful, returning without moving robot")
             return
 
-        # rospy.loginfo("ik was successful, returning joint position")
         return response.joint_state.position
 
     def exploreDrawer(self):
-        new_points = []
-        for (i, point) in enumerate(self.joint_knot_points):
+        for i, joint_position in enumerate(self.good_joint_positions):
             if not self.should_stop and not self.should_switch_modes:
-                self.cur_cart_point = self.all_cartesian_points[i]
-                self.cur_direction = self.directions[i]
-                self.robotService.moveToJointPosition(point, self.maxJointDegreesPerSecond)
-            elif self.should_stop:
+                self.cur_cartesian_point = self.good_cartesian_points[i]
+                self.cur_direction = self.good_directions[i]
+                self.robot_service.moveToJointPosition(joint_position, self.max_joint_deg_per_sec)
+            else:
                 break
-            elif self.should_switch_modes:
-                new_points = self.calcNextFindObjectPoints(self.cur_cart_point, self.cur_direction)
-                for point in new_points:
-                    if self.should_stop:
-                        break
-                    self.robotService.moveToJointPosition(point, self.maxJointDegreesPerSecond)
-                break
+        
         while not rospy.is_shutdown():
             if self.should_stop:
                 continue
+
             if self.should_switch_modes:
-                new_points = self.calcNextFindObjectPoints(self.cur_cart_point, self.cur_direction)
+                search_points = self.calcNextFindObjectPoints(self.cur_cartesian_point, self.cur_direction)
                 self.should_switch_modes = False
-                for point in new_points:
+
+                for point in search_points:
                     if self.should_stop or self.should_switch_modes:
                         break
-                    self.robotService.moveToJointPosition(point, self.maxJointDegreesPerSecond)
+                        
+                    self.cur_cartesian_point = point
+                    joint_position = self.getJointPositions(point, self.cur_direction)
+
+                    
+
+                    self.robot_service.moveToJointPosition(joint_position, self.max_joint_deg_per_sec)
+                    self.in_contact = False
             
-        # for point in new_points:
-        #     if self.should_stop:
-        #         break
-        #     self.robotService.moveToJointPosition(point, self.maxJointDegreesPerSecond)
         print "done"
 
 '''
 How to run this demo:
 
-Once spartan is running, open up a new terminal window (remember to run use_ros and use_spartan
+Once the spartan docker is running, open up a new terminal window (remember to run use_ros and use_spartan
 in each new window you open). In the main spartan directory, run
 
-python modules/spartan/manipulation/explore_object_standalone.py
+python modules/spartan/manipulation/explore_drawer.py
 
 This will start the robot through the demo of altenating between going to the given home
 point and every point in the touch points list. If at any time you want to stop the robot from
@@ -314,18 +272,6 @@ rostopic pub /stop std_msgs/Bool true
 
 If you want to send this command programmatically, make a publisher that publishes a boolean message
 to the /stop topic with data=true.
-
-
-
-Things to change in this file:
-
-- The points that are hardcoded in the main function below can be read in from anywhere else, including
-from a ROS topic or a ROS param file.
-
-- The getJointPositions method takes in the points as a list of 3 numbers, but that can be changed
-to match whatever format the points are input in.
-
-- maxJointDegreesPerSecond should be tuned based on testing on the real robot.
 
 '''
 def main():
